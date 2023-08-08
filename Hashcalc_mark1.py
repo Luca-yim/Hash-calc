4038e3c0a081bd199b5cf1284630fc861dc51cea
Luca-yim <82668646+Luca-yim@users.noreply.github.com>
2023-08-08 11:00:12 +0100

    Add files via upload
    made code easier to read and understand

--- Hashcalc_mark1.py
+++ Hashcalc_mark1.py
@@ -1,12 +1,16 @@
 import math
-entry = input("Enter a word: ")
+
 #get the word
+entry = input("Enter a word: ")
+
+#changing string to number
 x = range(len(entry))
 i = 0
 for n in x:
     i = ord(entry[n]) + i
 data_num = i
-#changing string to number
+
+#using multiplication method to hash entry
 A = 0.2072002
 M = 100
 rep = math.floor(M * (data_num * A % 1))
